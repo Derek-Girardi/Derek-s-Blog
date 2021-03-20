1edb826f429a703e866c66cbb1e28c5d12ef796c
Derek Girardi <girardiderek@gmail.com>
2021-03-20 18:19:49 -0400

    Some basic fields and coloring for login screen
    Created some empty functions to separate each element out on its own. Still a WIP but commiting what I currently have so incase something happens, I dont lose it.

--- lib/UI/HomeScreen.dart
+++ lib/UI/HomeScreen.dart
@@ -1,86 +1,156 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
-  HomeScreen({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        body: Stack(
+      children: <Widget>[
+        Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                Color(0xff354651),
+                Color(0xff354047),
+                Color(0xff33383D),
+                Color(0xff323232),
+              ],
+                  stops: [
+                0.1,
+                0.4,
+                0.7,
+                0.9
+              ])),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            margin: EdgeInsets.symmetric(horizontal: 20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'Sign In',
+                  style: TextStyle(
+                    color: Colors.amber,
+                    fontFamily: 'OpenSans',
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 40.0),
+                _emailField(),
+                SizedBox(height: 40.0),
+                _passwordField(),
+                _forgotPasswordBtn(),
+                //  _rememberMe(),
+                //  _loginBtn(),
+                //  _signInTextSeparator(),
+                // _otherLoginBtns(),
+                // _signupBtn(),
+                //other fucntions
+              ],
+            ))
+      ],
+    ));
   }
 }
+
+Widget _emailField() {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+      Text(
+        'Email',
+        style: TextStyle(
+          color: Colors.amber,
+          fontFamily: 'OpenSans',
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      SizedBox(height: 10.0),
+      Container(
+          decoration: BoxDecoration(
+              color: Color(0xFF6CA8F1),
+              borderRadius: BorderRadius.circular(12),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.black12,
+                    blurRadius: 6.0,
+                    offset: Offset(0, 2))
+              ]),
+          alignment: Alignment.centerLeft,
+          child: TextField(
+            keyboardType: TextInputType.emailAddress,
+            style: TextStyle(
+              color: Colors.amber,
+              fontFamily: 'OpenSans',
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ))
+    ],
+  );
+}
+
+Widget _passwordField() {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+      Text(
+        'Password',
+        style: TextStyle(
+          color: Colors.amber,
+          fontFamily: 'OpenSans',
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      SizedBox(height: 10.0),
+      Container(
+          alignment: Alignment.centerLeft,
+          //decoration: BoxDecoration(
+
+          //  ),
+          child: TextField(
+            decoration: InputDecoration(
+              labelText: 'Enter your password',
+              labelStyle: TextStyle(
+                color: Colors.amber,
+                fontFamily: 'OpenSans',
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ))
+    ],
+  );
+}
+
+Widget _forgotPasswordBtn() {
+  return Container(
+    alignment: Alignment.centerRight,
+    child: TextButton(
+      onPressed: () =>
+       print('press me') ,
+      child: Text(
+        'Forgot Password?'
+      ),
+  ));
+}
+
+Widget _rememberMe() {}
+Widget _loginBtn() {}
+Widget _signInTextSeparator() {}
+Widget _otherLoginBtns() {}
+Widget _signupBtn() {}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:personal_flutter_blog/main.dart';
+import 'package:derek_blog/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
